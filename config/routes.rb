15812795c78651bbd0036370ee700e0763ec5f59
Gabriel Manrique <alberto.manrique99@gmail.com>
2024-02-26 17:11:03 -0400

    Bien

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
 
   #Rutas del Controlador del marcador de libros
   get 'lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: 'new_list_bookmark'
-  post 'lists/:list_id/bookmarks', to: 'bookmarks#create'
+  post 'lists/:list_id/bookmarks', to: 'bookmarks#create', as: 'list_bookmarks'
   delete 'bookmarks/:id', to: 'bookmarks#destroy', as: 'bookmark'
 end
